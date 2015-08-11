c3db437deb02a94f89449206db7ba62e50dd1721
Adam Houston <phazyy9@gmail.com>
2015-08-11 17:26:09 +0100

    FAB tweaked and added a dark theme
    Added labels to tbe Floating Action Button and created a dark theme which actives at night-time

--- js/material.js
+++ js/material.js
@@ -1,15 +1,34 @@
+// Handling the page Clock
 function GetClock() {
+
+	// Get the time and edit the html
 	d = new Date();
-	nhour  = d.getHours();
-	nmin   = d.getMinutes();
-	nsec   = d.getSeconds();
-	if(nmin <= 9) {nmin = "0" +nmin;}
-	if(nsec <= 9) {nsec = "0" +nsec;}
-	document.getElementById('clock').innerHTML=""+nhour+":"+nmin+":"+nsec+"";
+	hour  = d.getHours();
+	min   = d.getMinutes();
+	sec   = d.getSeconds();
+	if (min <= 9)
+		min = "0" +min;
+	if (sec <= 9)
+		sec = "0" +sec;
+	document.getElementById('clock').innerHTML=""+hour+":"+min+":"+sec+"";
+
+	// If it's after 5PM and before 6AM use the dark theme
+	if (hour >= 17 || hour <= 6)
+		ChangeStylesheet('css/material-dark.css');
+	else
+		ChangeStylesheet('css/material.css');
+
+	// Refresh every 1000ms (1 second)
 	setTimeout("GetClock()", 1000);
 }
 window.onload=GetClock;
 
+// Change the active Material Stylesheet
+function ChangeStylesheet(link) {
+	document.getElementById('materialcss').setAttribute('href', link);
+}
+
+// Hover Listener for the Floating Action Button
 var fab = document.getElementById('fab');
 function fabEnable() {
     fab.classList.add('fab-active');
