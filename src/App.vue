bd24c4fd3783eb09bd8e8ca2b5af8edd4a847aec
Adam Houston <phazyy9@gmail.com>
2019-02-24 20:13:51 +0000

    Adding basic toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,6 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <Toolbar />
 
     <v-content>
       <HelloWorld/>
@@ -23,11 +10,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import Toolbar from './components/Toolbar'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Toolbar
   },
   data () {
     return {
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-toolbar id="core-toolbar" flat prominent style="background: #eee;">
+    <v-toolbar-title class="tertiary--text font-weight-light" >
+      {{ title }}
+      <v-btn class="default v-btn--simple" icon @click.stop="onClickBtn">
+        <v-icon>arrow_drop_down</v-icon>
+      </v-btn>
+    </v-toolbar-title>
+
+    <v-text-field
+      flat
+      solo
+      hide-details
+      prepend-inner-icon="search"
+      label="Search"
+      class="hidden-sm-and-down"
+    ></v-text-field>
+
+    <v-spacer />
+
+    <v-btn icon>
+      <router-link v-ripple class="toolbar-items" to="/">
+        <v-icon color="tertiary">dashboard</v-icon>
+      </router-link>
+    </v-btn>
+    <v-btn icon>
+      <router-link v-ripple class="toolbar-items" to="/user-profile">
+        <v-icon color="tertiary">person</v-icon>
+      </router-link>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    title: 'Google'
+  })
+}
+</script>
+
+<style>
+  #core-toolbar a {
+    text-decoration: none;
+  }
+</style>
